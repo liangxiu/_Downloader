80bedf59c903b0cae7c8d37c16fdf7e848b06bb9
traview <traview@tuwentingdeiMac.local>
2016-03-17 15:19:11 +0800

    avoid network error when parsing the webpages

--- fetch_video_info.py
+++ fetch_video_info.py
@@ -2,6 +2,7 @@
 import requests
 import bs4
 import re
+import time
 
 root_url = 'https://www.youtube.com/'
 user_url = root_url + 'user/'
@@ -9,21 +10,11 @@ user_list_suffix = '/playlists'
 list_url = root_url + 'playlist?list='
 
 def get_play_list(author):
-	try:
-		response = requests.get(user_url + author + user_list_suffix)
-	except Exception as e:
-		print("Error!!! %s" % e)
-		return []
-	soup = bs4.BeautifulSoup(response.text, 'html.parser')
+	soup = bs4.BeautifulSoup(get_remote_response(user_url + author + user_list_suffix), 'html.parser')
 	return [a.attrs.get('href') for a in soup.select('div.yt-lockup-content a[href^="/playlist?"]')]
 
 def get_watch_list(list_suffix):
-	try:
-		response = requests.get(root_url + list_suffix.replace("/",""))
-	except Exception as e:
-		print("Error!!! %s" % e)
-		return []
-	soup = bs4.BeautifulSoup(response.text, 'html.parser')
+	soup = bs4.BeautifulSoup(get_remote_response(root_url + list_suffix.replace("/","")), 'html.parser')
 	hrefs = soup.select('a[href^="/watch?"]')
 	print("count: %d" % len(hrefs))
 	watchs = set()
@@ -35,12 +26,7 @@ def get_watch_list(list_suffix):
 
 def get_video_for_watch(watch_suffix):
 	url = root_url + watch_suffix
-	try:
-		response = requests.get(url)
-	except Exception as e:
-		print("Error!!! %s" % e)
-		return
-	html = response.text
+	html = get_remote_response(url)
 	count_s = re.findall(r'<meta itemprop="interactionCount" content="(\d+)">', html)
 	count = int(count_s[0])
 	title_s = re.findall(r'"title":"(.*?)"', html)
@@ -49,5 +35,14 @@ def get_video_for_watch(watch_suffix):
 	dist_date = dist_s[0]
 	return [title, dist_date, count]
 
+def get_remote_response(url): 
+	while(True):
+		try:
+                	response = requests.get(url)
+			break
+        	except Exception as e:
+                	print("Error!!! for url: %s with reason:\n%s" % (url, e))
+           		time.sleep(60)     
+	return response.text
 #print(get_view_count_for_watch('watch?v=19kPdeqJxYU'))
 #print(get_play_lists('itsJudysLife'))
